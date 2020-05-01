e63f67a62bd06bb3e78a120c8d8384dc114a510c
ClassroomAccount <jpepe44@gmail.com>
2020-05-01 18:29:54 -0400

    Initial commit

---
+++ app/src/main/java/com/course/example/customarrayadapter/CustomAdapter.java
@@ -0,0 +1,36 @@
+package com.course.example.customarrayadapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+class CustomAdapter extends ArrayAdapter {
+    ArrayList<SubjectData> arrayList;
+    Context context;
+    public CustomAdapter(Context context, ArrayList<SubjectData> arrayList) {
+        super(context, 0, arrayList);
+        this.arrayList=arrayList;
+        this.context=context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        SubjectData subjectData=arrayList.get(position);
+        if(convertView==null){
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            convertView=layoutInflater.inflate(R.layout.list_row, null);
+
+            TextView tittle=convertView.findViewById(R.id.title);
+            ImageView imag=convertView.findViewById(R.id.list_image);
+            tittle.setText(subjectData.getSubjectName());
+            imag.setImageResource(subjectData.getImage());
+
+        }
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/course/example/customarrayadapter/MainActivity.java
@@ -0,0 +1,47 @@
+package com.course.example.customarrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    public ArrayList<SubjectData> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView list = findViewById(R.id.list);
+
+        //attach listener
+        list.setOnItemClickListener(this);
+
+        arrayList = new ArrayList<SubjectData>();
+        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", R.drawable.java));
+        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", R.drawable.python));
+        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", R.drawable.javascript));
+        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", R.drawable.c));
+        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", R.drawable.cpp));
+        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", R.drawable.android));
+
+        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+        list.setAdapter(customAdapter);
+    }
+
+    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+        String link = arrayList.get(position).getLink();
+
+        //open link in browser
+        Uri uri = Uri.parse(link);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+}
+
