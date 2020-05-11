1ca0b1dd8643525be5d795deff90664b32447904
ClassroomAccount <jpepe44@gmail.com>
2020-05-11 16:17:37 -0400

    add dart and lisp

--- app/src/main/java/com/course/example/customarrayadapter/MainActivity.java
+++ app/src/main/java/com/course/example/customarrayadapter/MainActivity.java
@@ -24,12 +24,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         list.setOnItemClickListener(this);
 
         arrayList = new ArrayList<SubjectData>();
+        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", R.drawable.android));
         arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", R.drawable.java));
         arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", R.drawable.python));
         arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", R.drawable.javascript));
         arrayList.add(new SubjectData("C", "https://www.tutorialspoint.com/cprogramming/", R.drawable.c));
         arrayList.add(new SubjectData("C++", "https://www.tutorialspoint.com/cplusplus/", R.drawable.cpp));
-        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", R.drawable.android));
+        arrayList.add(new SubjectData("Dart", "https://www.tutorialspoint.com/dart_programming/", R.drawable.dart));
+        arrayList.add(new SubjectData("Lisp", "https://www.tutorialspoint.com/lisp/", R.drawable.lisp));
 
         CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
         list.setAdapter(customAdapter);
